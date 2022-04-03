d89145b99b14b0b07a98cb67060b5357b92084cd
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-04-03 15:04:13 +1000

    restructer html and JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,8 @@
-var startButton = document.querySelector(".start-button");
-var h1El = document.createElement("h1");
-let startBtn = document.createElement("button");
-var text = document.createTextNode("Start Quiz");
-let divBtn = document.createElement("divBtn");
-var divTimer = document.createElement("timer");
-var divClock = document.createElement("divclock");
-var divQuiz = document.createElement("divQuiz");
-var divResult = document.createElement("divResult");
-var rules = document.querySelector(".rules");
-// document.getElementById("quizRules").style.display = "none";
-
+var startBtn = document.querySelector(".startBtn button");
+var timerClock = document.getElementById("timerClock");
 
 var secondLeft = 60;
 var timer;
-h1El.textContent = "Code Quiz";
-divClock.textContent ="Time";
-
 
 var myQuestions = [
     {
@@ -52,41 +39,13 @@ var myQuestions = [
     ];
 
 
-
-document.body.appendChild(h1El);
-document.body.appendChild(divBtn);
-document.body.appendChild(divClock);
-document.body.appendChild(divQuiz);
-document.body.appendChild(divResult);
-startBtn.appendChild(text);
-document.body.appendChild(startBtn);
-document.body.appendChild(divTimer);
-document.getElementsByClassName(divBtn);
-
-
-
-h1El.classList.add("quiz-title");
-startBtn.classList.add("startBtn");
-divBtn.classList.add("divBtn");
-divTimer.classList.add("timerClock");
-divClock.classList.add("clockCl");
-divQuiz.classList.add("quizCon");
-
-h1El.setAttribute("style", "font-size: 50px; font-wiegth: bold; display: flex; flex-wrap: wrap; justify-content: center; text-align: center;  color: red;");
-startBtn.setAttribute("style", "display: flex; flex-wrap: wrap; justify-content: center;  text-align: center; font-size: 25px; font-weight: 500; background: #557A95; color: #5D5C61; padding: 15px 30px; border-radius: 5px;");
-divClock.setAttribute("style", "display: flex; flex-wrap: wrap; justify-content: flex-end;");
-divQuiz.setAttribute("id", "quiz");
-divBtn.setAttribute("id", "div-1");
-divClock.setAttribute("id", "clock");
-divTimer.setAttribute("id", "divtime");
-
 function start() {
-    timer = setInterval(callBack,1000)
+  timer = setInterval(callBack,1000)
 }
 
 function callBack() {
     secondLeft--;
-    divClock.textContent = "Time: " + secondLeft;
+    // timer.textContent = secondLeft;
 
 
     if (secondLeft === 0) {
@@ -110,6 +69,11 @@ function showQuestion() {
 
 document.querySelector("startBtn");
 startBtn.addEventListener("click", start);
+
+
+
+
+
 
 // function buildQuiz() {
 // // variable to store the HTML output
@@ -136,3 +100,44 @@ startBtn.addEventListener("click", start);
 
 // document.body.appendChild(startBtn);
 // startBtn.innerHTML = "Start";
+
+// var h1El = document.createElement("h1");
+// let startBtn = document.createElement("button");
+// var text = document.createTextNode("Start Quiz");
+// let divBtn = document.createElement("divBtn");
+// var divTimer = document.createElement("timer");
+// var divClock = document.createElement("divclock");
+// var divQuiz = document.createElement("divQuiz");
+// var divResult = document.createElement("divResult");
+// var rules = document.querySelector(".rules");
+// document.getElementById("quizRules").style.display = "none";
+
+// h1El.textContent = "Code Quiz";
+// divClock.textContent ="Time";
+
+// document.body.appendChild(h1El);
+// document.body.appendChild(divBtn);
+// document.body.appendChild(divClock);
+// document.body.appendChild(divQuiz);
+// document.body.appendChild(divResult);
+// startBtn.appendChild(text);
+// document.body.appendChild(startBtn);
+// document.body.appendChild(divTimer);
+// document.getElementsByClassName(divBtn);
+
+
+
+// h1El.classList.add("quiz-title");
+// startBtn.classList.add("startBtn");
+// divBtn.classList.add("divBtn");
+// divTimer.classList.add("timerClock");
+// divClock.classList.add("clockCl");
+// divQuiz.classList.add("quizCon");
+
+// h1El.setAttribute("style", "font-size: 50px; font-wiegth: bold; display: flex; flex-wrap: wrap; justify-content: center; text-align: center;  color: red;");
+// startBtn.setAttribute("style", "display: flex; flex-wrap: wrap; justify-content: center;  text-align: center; font-size: 25px; font-weight: 500; background: #557A95; color: #5D5C61; padding: 15px 30px; border-radius: 5px;");
+// divClock.setAttribute("style", "display: flex; flex-wrap: wrap; justify-content: flex-end;");
+// divQuiz.setAttribute("id", "quiz");
+// divBtn.setAttribute("id", "div-1");
+// divClock.setAttribute("id", "clock");
+// divTimer.setAttribute("id", "divtime");
